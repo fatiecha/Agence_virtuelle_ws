252c8baa41ac60834b4d59b9ad01227d2c036b89
fatiecha <fati.echarqaoui@gmail.com>
2017-03-27 18:18:20 +0100

    commit3

--- src/projectWS/projectWS/Test.java
+++ src/projectWS/projectWS/Test.java
@@ -14,14 +14,13 @@ import bean.Reclamation;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
-		List<Echeance> l = new ArrayList<Echeance>();
-ConsulterFacilitesWS c=new ConsulterFacilitesWS();
-		l = c.consulterEcheance("1");
+		List<Reclamation> l = new ArrayList<Reclamation>();
+ConsulterReclamationsWS c=new ConsulterReclamationsWS();
+		l = c.consulterReclamation("1");
 		System.out.println(l);
 		for (int i = 0; i < l.size(); i++) {
-			Echeance d = l.get(i);
-			System.out.println(d.getOrdre());
-			System.out.println(d.getMontant());
+			Reclamation d = l.get(i);
+			System.out.println(d.getCommentaire());
 		}
 
 	}
