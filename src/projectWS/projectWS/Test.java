9a5243c9859f16c4cb7155f03b50fb394081a765
fatiecha <fati.echarqaoui@gmail.com>
2017-04-13 16:33:59 +0100

    update

--- src/projectWS/projectWS/Test.java
+++ src/projectWS/projectWS/Test.java
@@ -31,9 +31,25 @@ public class Test {
 //		System.out.println(a);
 //		String msg=c.addContratClient("333", "1", "eau");
 //		System.out.println(msg);
-		AjouterDemAboWS c=new AjouterDemAboWS();
-		String msg=c.ajouterDem_Abo("1555", "1", "eau", "tar");
-		System.out.println(msg);
+//		AjouterDemAboWS c=new AjouterDemAboWS();
+//		String msg=c.ajouterDem_Abo("1555", "1", "eau", "tar");
+//		System.out.println(msg);
+//		ConsulterContratsWS c=new ConsulterContratsWS();
+//		List<Contrat> l=new ArrayList<Contrat>();
+//		l=c.consulterContrats("1");
+//		for(int i=0;i<l.size();i++){
+//			Contrat e =l.get(i);
+//			System.out.println(e.getId());
+//		}
+		
+		ConsulterCompteWS c=new ConsulterCompteWS();
+		Compte d=new Compte();
+		d=c.consulterCompte("12");
+		System.out.println(d.getCredit());
+		System.out.println(d.getProvision());
+		System.out.println(d.getMt_impayé());
+		System.out.println(d.getMt_exigible());
+
 }
 
 }
